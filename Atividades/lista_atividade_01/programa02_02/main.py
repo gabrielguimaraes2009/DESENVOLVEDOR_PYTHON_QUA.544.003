0eeb0883f457fc492f312b018fe1d1824c6ca4b2
gabrielguimaraes2009 <gg5172026@gmail.com>
2026-07-27 21:44:34 -0300

    Aula do dia 27/07/2026

--- Atividades/lista_atividade_01/programa02_02/main.py
+++ Atividades/lista_atividade_01/programa02_02/main.py
@@ -3,7 +3,7 @@ idade = int(input("Digite sua idade: "))
 
 filmes = {
     1: ("A Volta dos Que Não Foram", 0),
-    2: ("A Doda Quadra", 12),
+    2: ("A Roda Quadra", 12),
     3: ("As Tranças do Rei Careca", 14),
     4: ("Poeira em Alto Mar", 16),
     5: ("A Vingança do Frango Assado", 18)
@@ -12,7 +12,7 @@ filmes = {
 while True:
     print("\n=== FILMES ===")
     print("1 - A Volta dos Que Não Foram (Livre)")
-    print("2 - A Doda Quadra (12 anos)")
+    print("2 - A Roda Quadra (12 anos)")
     print("3 - As Tranças do Rei Careca (14 anos)")
     print("4 - Poeira em Alto Mar (16 anos)")
     print("5 - A Vingança do Frango Assado (18 anos)")
@@ -21,9 +21,8 @@ while True:
 
     if opcao in filmes:
         filme, classificacao = filmes[opcao]
-
         if idade >= classificacao:
-            with open("bilhete.txt", "w", encoding="utf-8") as arquivo:
+            with open(f"programa02_02/bilhetes/{nome}.txt", "w", encoding="utf-8") as arquivo:
                 arquivo.write("===== BILHETE =====\n")
                 arquivo.write(f"Nome: {nome}\n")
                 arquivo.write(f"Filme: {filme}\n")
@@ -36,4 +35,5 @@ while True:
             print("\nVocê não tem idade suficiente para esse filme.")
             print("Escolha outro filme.")
     else:
-        print("Opção inválida!")
+        print("Opção inválida! 😊😛🙈🐶👩‍❤️‍👩👤👩🏾‍🤝‍👩🏻👩🏽‍🤝‍👩🏼🎃👓")
+        
