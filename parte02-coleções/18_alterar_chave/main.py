7082eed2de84b5ad4304470093f4d3770612818f
unknown <andre.roglemn@gmail.com>
2026-08-05 21:39:36 -0300

    Aula do dia 05/06/2026

---
+++ parte02-coleções/18_alterar_chave/main.py
@@ -0,0 +1,11 @@
+usuario = {
+    'nome': "Fulano de tal", 
+    'idade':15,
+    'email': "fulano@gmail.com",
+    'cpf': "123-456-789-12"
+}
+
+# usuario informa a chave que deseja alterar
+chave = input("Informe o nome da chave: ").strip().lower()
+
+# TODO: verifica se a chave existe
