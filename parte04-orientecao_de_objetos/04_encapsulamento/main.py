c70a2e41b7780d3a54b151c4130f7e09a1b693c4
unknown <andre.roglemn@gmail.com>
2026-08-24 21:43:57 -0300

    Aula do dia 24/08/2026

--- parte04-orientecao_de_objetos/04_encapsulamento/main.py
+++ parte04-orientecao_de_objetos/04_encapsulamento/main.py
@@ -1,24 +1,40 @@
 import os
 
-from models import Pessoa
+from models import PessoaFisica, PessoaJuridica
 
 def limpar():
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system("cls" if os.name == "nt" else "clear")
 
 def main():
+    usuario = PessoaFisica(nome="",cpf="",email="",telefone="")
+    empresa = PessoaJuridica(nome_fantasia="",cnpj="",email="",telefone="")
+
     limpar()
 
-    usuario = Pessoa(nome="", cpf="", email="", telelfone="")
+    usuario.nome = input("Informe o nome do usuário: ").strip().title()
+    usuario.cpf = input("Informe o CPF do usuário: ").strip()
+    usuario.email = input("Informe o e-mail do usuário: ").strip().lower()
+    usuario.telefone = input("Informe o telefone do usuário: ").strip()
 
-    usuario.nome = input("Informe o nome: ").strip().title()
-    usuario.cpf = input("Informe o CPF: ").strip()
-    usuario.email = input("Informe o email: ").strip().lower()
-    usuario.telefone = input("Informe o telefone: ").strip()
+    limpar()
 
-    print(f"Nome: {usuario.nome}")
-    print(f"CPF: {usuario.cpf}")
-    print(f"Email: {usuario.email}")
-    print(f"Telefone: {usuario.telefone}")
+    empresa.nome_fantasia = input("Informe o nome da empresa: ").strip()
+    empresa.cnpj = input("Informe o CNPJ da empresa: ").strip()
+    empresa.email = input("Informe o e-mail da empresa: ").strip().lower()
+    empresa.telefone = input("Informe o telefone da empresa: ").strip()
+
+    limpar()
+
+    print(f"Nome do usuário: {usuario.nome}")
+    print(f"CPF do usuário: {usuario.cpf}")
+    print(f"E-mail do usuário: {usuario.email}")
+    print(f"Telefone do usuário: {usuario.telefone}")
+
+    print(f"Nome da empresa: {empresa.nome_fantasia}")
+    print(f"CNPJ da empresa: {empresa.cnpj}")
+    print(f"E-mail da empresa: {empresa.email}")
+    print(f"Telefone da empresa: {empresa.telefone}")
+
 
 if __name__ == "__main__":
-        main()
+    main()
--- parte04-orientecao_de_objetos/04_encapsulamento/models.py
+++ parte04-orientecao_de_objetos/04_encapsulamento/models.py
@@ -1,26 +1,66 @@
 class Pessoa:
-    def __init__(self, nome, telefone, cpf, email):
-        self.__nome = nome 
-        self.__cpf = cpf
+    def __init__(self,email,telefone):
         self.__email = email
         self.__telefone = telefone
 
-    # métodos de acesso
+    @property
+    def email(self):
+        return self.__email
+
+    @email.setter
+    def email(self,email):
+        self.__email = email
+
+    @property
+    def telefone(self):
+        return self.__telefone
+
+    @telefone.setter
+    def telefone(self,telefone):
+        self.__telefone = telefone
+
+
+class PessoaFisica(Pessoa):
+    def __init__(self,nome,cpf,email,telefone):
+        self.__nome = nome
+        self.__cpf = cpf
+        super().__init__(email=email,telefone=telefone)
+
     @property
     def nome(self):
         return self.__nome
 
     @nome.setter
-    def nome(self, nome):
+    def nome(self,nome):
         self.__nome = nome
 
     @property
-    def idade(self):
-        return self.__idade
+    def cpf(self):
+        return self.__cpf
 
-    @idade.setter
-    def idade(self, nova_idade):
-        if isinstance(nova_idade, int) and nova_idade >= 0:
-            self.__idade = nova_idade
-        else:
-            raise ValueError("Idade deve ser um número inteiro não negativo.")
+    @cpf.setter
+    def cpf(self,cpf):
+        self.__cpf = cpf
+
+
+class PessoaJuridica(Pessoa):
+    def __init__(self,nome_fantasia,cnpj,email,telefone):
+        self.__nome_fantasia = nome_fantasia
+        self.__cnpj = cnpj
+        super().__init__(email=email,telefone=telefone)
+
+    @property
+    def nome_fantasia(self):
+        return self.__nome_fantasia
+
+    @nome_fantasia.setter
+    def nome_fantasia(self,nome_fantasia):
+        self.__nome_fantasia = nome_fantasia
+
+    @property
+    def cnpj(self):
+        return self.__cnpj
+
+    @cnpj.setter
+    def cnpj(self,cnpj):
+        self.__cnpj = cnpj
